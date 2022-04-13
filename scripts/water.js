1339c52b5ee642d2dcfc1d533326f0fe39a84300
Excel <excelthebeanman@gmail.com>
2022-04-13 18:42:46 +0200

    g

--- scripts/water.js
+++
@@ -1,9 +0,0 @@
-import { Water } from 'https://threejs.org/examples/jsm/objects/Water.js';
-
-function buildScene() {
-    const sceene = new THREE
-}
-
-function SceneManager(canvas) {
-    // Magic goes here
-}
